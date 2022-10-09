bac80dde38de1641dd6eeae96bba1f277d8c7656
aceauses <andrei752@yahoo.com>
2022-10-09 14:11:56 +0300

    Prototype

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-x = int(input("F1 number: "))
-z = int(input("F2 number: "))
+import cv2 as cv
 
-print(x + z)
+# Define video capture
+vid = cv.VideoCapture(0)
+#Check if camera can be opened
+if not vid.isOpened():
+    print("Camera cannot be opened..")
+    exit()
+while True:
+    ret, frame = vid.read()
+    if not ret:
+        print("Cannot read frame of the capture")
+        break
+    cv.imshow('recognision', frame)
+    print("FPS: ", cv.CAP_PROP_FPS)
+    #break loop
+    if cv.waitKey(1) == ord('q'):
+        break
+
+vid.release()
+cv.destroyAllWindows()
+#Printing FPS
+#print("FPS: ", cv.CAP_PROP_FPS)
